12e94520fa26a2b3dd617090c271197c7b51c05c
Eka Yunan <ekayunan98@gmail.com>
2024-08-07 21:15:50 +0700

    feat(Models): Make model scheme

--- app/Models/Recipe.php
+++ app/Models/Recipe.php
@@ -2,10 +2,56 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Recipe extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'thumbnail',
+        'about',
+        'url_file',
+        'url_video',
+        'category_id',
+        'recipe_author_id'
+    ];
+
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function photos(): HasMany
+    {
+        return $this->hasMany(RecipePhoto::class);
+    }
+
+    public function tutorials(): HasMany
+    {
+        return $this->hasMany(RecipeTutorial::class);
+    }
+
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(RecipeAuthor::class);
+    }
+
+    public function recipeIngredients(): HasMany
+    {
+        return $this->hasMany(RecipeIngredient::class);
+    }
 }
