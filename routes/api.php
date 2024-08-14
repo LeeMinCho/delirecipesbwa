04a3b77205f2f0b92457860770b34a1a0c1cfb5c
Eka Yunan <ekayunan98@gmail.com>
2024-08-14 19:09:57 +0700

    feat(Search Endpoint): new Endpoint

--- routes/api.php
+++ routes/api.php
@@ -2,12 +2,15 @@
 
 use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\RecipeController;
+use App\Http\Controllers\Api\SearchController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/recipe/search', [SearchController::class, 'index']);
 
 Route::get('/category/{category:slug}', [CategoryController::class, 'show']);
 Route::apiResource('/categories', CategoryController::class);
